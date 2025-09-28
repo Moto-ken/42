2627b8200280557c8eb69193fff478926474effc
Moto-Ken <motoken01315@gmail.com>
2025-09-29 04:07:19 +0900

    WIP: 作業途中の変更を一時コミット

---
+++ get_next_line/main.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kemotoha <kemotoha@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 10:00:00 by kemotoha          #+#    #+#             */
+/*   Updated: 2025/01/15 10:00:00 by kemotoha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv)
+{
+    int     fd;
+    char    *line;
+
+    if (argc != 2)
+    {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return (1);
+    }
+
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1)
+    {
+        printf("Error: Cannot open file %s\n", argv[1]);
+        return (1);
+    }
+
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("%s", line);
+        free(line);
+    }
+
+    close(fd);
+    return (0);
+} 
